ca1c4835024c2b033041f217aef8f7a6aea8b413
Prasad Gurude <prasadgurude76@gmail.com>
2025-02-22 12:27:36 +0530

    user login route is complete

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -34,3 +34,33 @@ module.exports.registerUser = async (req, res, next) => {
         res.status(400).send(e);
     }
 }
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isPasswordMatch = await user.comparePassword(password);
+
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: 'Invalid password' });
+        }
+
+        const token = user.generateAuthToken();
+
+        res.status(200).json({ user, token });
+
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+}
