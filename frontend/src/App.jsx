0cdea79abff7bd20aac0a22c7d22f6b5cb9c1bf9
Prasad Gurude <prasadgurude76@gmail.com>
2025-02-24 00:36:29 +0530

    basic compnent files are created and linked with the routes using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/Captainlogin'
+import Captainsignup from './pages/Captainsignup'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<Captainlogin />} />
+        <Route path="/captain-signup" element={<Captainsignup />} />
+      </Routes>
+    </div>
   )
 }
 
