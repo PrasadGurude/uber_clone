7f72aaccee46cf9e6e5f7bc8f8f8465f0ae402ff
Prasad Gurude <prasadgurude76@gmail.com>
2025-02-27 00:04:59 +0530

    home page frotend is done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
 
 const Home = () => {
 
   const [pickup, setPickup] = useState('')
   const [destination, setDestination] = useState('')
   const [panelOpen, setPanelOpen] = useState(false)
+  const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
+
+  useGSAP(function () {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '70%',
+        // opacity:1
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:1
+      })
+    } else {
+      gsap.to(panelRef.current, {
+        height: "0%",
+        // opacity:0
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:0
+      })
+    }
+  }, [panelOpen , panelCloseRef])
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -17,34 +42,45 @@ const Home = () => {
         <img className='h-full ' src="https://uploads.disquscdn.com/images/abf0ee2a43831d4bafc38d5d4e8671317af55edb03da2cc27b786a1ea2d8bb81.png" alt="" />
       </div>
       <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='bg-white p-4 h-[30%] relative'>
+        <div className='bg-white p-6 h-[30%] relative'>
+          <h5
+          ref={panelCloseRef}
+          onClick={()=>{
+            setPanelOpen(false)
+          }}  
+          className='absolute top-0 right-3 opacity-0 text-2xl'>
+          <i class="ri-arrow-down-wide-fill"></i>
+          </h5>
           <h4 className='font-bold text-2xl'>Find a trip</h4>
           <form onSubmit={(e) => {
             submitHandler(e)
           }}>
             <div className='line absolute h-12 w-1 top-[50%] bg-gray-800 rounded-2xl left-10'></div>
-            <input 
-            onClick={()=>{
-              setPanelOpen(true)
-            }}
-            value={pickup}
-            onChange={(e)=>{
-              setPickup(e.target.value)
-            }} 
-            className='w-full bg-gray-100 py-3 px-12 rounded-lg mt-3' 
-            type="text" 
-            placeholder='Add a pick-up location'/>
-            <input 
-            className='w-full bg-gray-100 py-3 px-12 rounded-lg mt-5' 
-            type="text" 
-            placeholder='Enter your destination'
-            value={destination}
-            onChange={(e)=>{
-              setDestination(e.target.value)
-            }} />
+            <input
+              onClick={() => {
+                setPanelOpen(true)
+              }}
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value)
+              }}
+              className='w-full bg-gray-100 py-3 px-12 rounded-lg mt-3'
+              type="text"
+              placeholder='Add a pick-up location' />
+            <input
+              onClick={() => {
+                setPanelOpen(true)
+              }}
+              className='w-full bg-gray-100 py-3 px-12 rounded-lg mt-5'
+              type="text"
+              placeholder='Enter your destination'
+              value={destination}
+              onChange={(e) => {
+                setDestination(e.target.value)
+              }} />
           </form>
         </div>
-        <div className=' bg-red-500 h-[0]'>
+        <div ref={panelRef} className='bg-red-500 opacity-0 h-[0]'>
 
         </div>
       </div>
